6d450e339c2b3e3549e9e356bfdd040f890993ad
pedro <pedro.gch01@gmail.com>
2019-06-21 01:31:24 -0300

    pagina casi lista!

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ app.set('view engine' , 'ejs');
 //routes
 app.use(require('./routes/'));
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'css')));
+app.use(express.static(path.join(__dirname, 'img')));
+
 //listening the server
 app.listen(app.get('port'), () => {
    console.log('Servidor en marcha! en el puerto', app.get('port'));
